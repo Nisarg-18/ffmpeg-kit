db02947d7e4b61d9ae1cddd8b00658432bcc2674
Ishaan Bajaj <ishaanbajaj12@gmail.com>
2026-02-17 17:21:33 -0500

    fix: outdated ffmpeg kit aar

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -4,8 +4,8 @@ APP_ABI := armeabi-v7a arm64-v8a
 
 APP_STL := none
 
-APP_PLATFORM := android-16
+APP_PLATFORM := android-24
 
-APP_CFLAGS := -O3 -DANDROID -DFFMPEG_KIT_LTS -DFFMPEG_KIT_BUILD_DATE=20250306 -Wall -Wno-deprecated-declarations -Wno-pointer-sign -Wno-switch -Wno-unused-result -Wno-unused-variable
+APP_CFLAGS := -O3 -DANDROID -DFFMPEG_KIT_BUILD_DATE=20260217 -Wall -Wno-deprecated-declarations -Wno-pointer-sign -Wno-switch -Wno-unused-result -Wno-unused-variable
 
 APP_LDFLAGS := -Wl,--hash-style=both
